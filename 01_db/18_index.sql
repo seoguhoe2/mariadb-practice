f66b1dfd70abe7c63b6e4476575e4eef33670225
seoguhoe2 <js8337349@naver.com>
2025-07-24 11:38:59 +0900

    수업 내용 커밋

--- 01_db/18_index.sql
+++ 01_db/18_index.sql
@@ -5,9 +5,10 @@ DROP TABLE if exists phone;
 CREATE TABLE if NOT exists phone(
     phone_number INTeger,
     phone_code INT PRIMARY key,
-    phone_name VARCHAR(100) unique,
-    phone_price INTEGER
+    phone_name VARCHAR(100),
+    phone_price INTEGER unique
 );
+CREATE INDEX idx_name ON phone(phone_name);
 
 INSERT
   INTO phone
@@ -34,8 +35,22 @@ explain SELECT * FROM phone WHERE phone_code = 1;
 explain SELECT * FROM phone WHERE phone_name = 'iphone17pro';
 explain SELECT * FROM phone WHERE phone_price = 1730000;
  
+-- 인덱스는 필요한 곳에 달아야 한다.
+-- 1) 조건으로 자주 활용할 column(where, having, join 사용할 때 쓰는 column들)일 수록  --
+-- 2) 데이터가 자주 수정되지 않는 컬럼일수록                                           --
+-- 3) '='을 사용하는 조건에 쓰는 컬럼일수록                                            --
+-- 4) 전체 데이터의 10%~15%정도일수록(경험적 수치)                                     -- 효율적이다.
+-- 5) 복합속성일 경우(컬럼이 2개 이상)는 Cardinality가 높은(중복이 적은) 컬럼을 사용해서 적어야 한다.
 
+-- 인덱스 단점
+-- 1) 별도의 저장공간 필요
+-- 2) 주기적으로 인덱스를 다시 업데이트 해야한다.
 
+-- 인덱스 최적화(업데이트)
+OPTIMIZE TABLE phone;       -- oracle에선 rebuild 명령어 사용
 
+-- 인덱스 삭제
+DROP index idx_name ON phone;
+SHOW INDEX FROM phone;
 
 
